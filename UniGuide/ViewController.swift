3f9f55fd6699d09e92bb99c6110f8073ebc45d70
“gokulnair2001” <gokulnair.2001@gmail.com>
2021-03-29 20:24:03 +0530

    first commit

---
+++ UniGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniGuide
+//
+//  Created by Gokul Nair on 29/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
