820b81ee3407f7def9feaf6469ad7b782ec54d6a
gokulnair2001 <gokulnair.2001@gmail.com>
2021-05-27 02:01:11 +0530

    pload section added

---
+++ UniGuide/pjTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  pjTableViewCell.swift
+//  UniGuide
+//
+//  Created by Gokul Nair on 27/05/21.
+//
+
+import UIKit
+
+class pjTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var pjNameLbl: UILabel!
+    @IBOutlet weak var pjImage: UIImageView!
+    @IBOutlet weak var yearlLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
